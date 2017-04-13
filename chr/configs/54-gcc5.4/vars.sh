f0426ba8f8272c71c5bff46a53aafb97cd2ad347
Farkhad Khatamov <khatamov@mera.ru>
2017-04-13 17:42:53 +0300

    add config for 54 version

---
+++ chr/configs/54-gcc5.4/vars.sh
@@ -0,0 +1,13 @@
+# Tag/branch/hash to buld chromium from
+CHROMIUM_REVISION="56.0.2924.122"
+
+# Path to cross compilation toolchain (inside container)
+# Use gcc 5.4 corss compilation toolchain installed into container
+PATH_TO_TOOCHAIN_BIN_DIR=""
+
+# Path to chromium sources (inside container)
+PATH_TO_CHROMIUM_SRC_DIR="/chr/chromium/src"
+
+# Path to target sysroot (inside container)
+PATH_TO_SYSROOT_DIR="/chr/sysroots/baikal-rootfs-chromedeps"
+
